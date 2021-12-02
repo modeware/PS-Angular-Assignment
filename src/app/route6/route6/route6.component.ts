f47e0e2c0e486fb0b395a36bcb69e00a359bfb45
Safiq <safiqlodi2@gmail.com>
2021-12-02 15:11:49 +0530

    Implemented Dynamic Div Scroll

--- src/app/route6/route6/route6.component.ts
+++ src/app/route6/route6/route6.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-route6',
   templateUrl: './route6.component.html',
   styleUrls: ['./route6.component.css']
 })
-export class Route6Component implements OnInit {
+export class Route6Component implements OnInit, AfterViewInit {
+  @ViewChild('containerdiv') containerdiv: any;
+  count:number = 1;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    
+  }
+  ngAfterViewInit(): void {
+
+    for(let i =0; i < 3; i++){
+      this.onWindowScroll()
+    }
   }
 
+  onWindowScroll(){
+    console.log('hello')
+    let count = this.count++;
+    let div = this.renderer.createElement('div');
+    let button = this.renderer.createElement('button')
+    button.textContent = 'Button'
+    div.appendChild(button)
+    this.renderer.addClass(div, 'box')
+    this.renderer.listen(button, 'click', ()=>{
+      (function(){let c = count;
+      alert(`Button in ${c}th div is clicked`)
+    }())
+    })
+
+    this.containerdiv.nativeElement.appendChild(div)
+
+  }
 }
